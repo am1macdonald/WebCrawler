b747514b0f43193d535dd428371458e5871afa70
Adam MacDonald <am1macdonald@gmail.com>
2024-02-28 20:40:06 -0500

    Refactor

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,39 +1,42 @@
-const {describe, expect, test} = require("@jest/globals");
-const {JSDOM} = require("jsdom")
-const {normalizeURL} = require("../normalizeURL");
-const {getURLsFromHTML} = require("../getURLsFromHTML");
+const { describe, expect, test } = require('@jest/globals');
+const { JSDOM } = require('jsdom');
+const { normalizeURL } = require('../normalizeURL');
+const { getURLsFromHTML } = require('../getURLsFromHTML');
 
 describe('Main can normalize URLS', () => {
-	const normalized = "blog.boot.dev/path";
-	test('Function normalizes urls', () => {
-		expect(normalizeURL("https://blog.boot.dev/path/wow")).toBe(normalized + '/wow');
-	});
-	test('Function normalizes urls', () => {
-		expect(normalizeURL("https://blog.boot.dev/path/")).toBe(normalized);
-	});
-	test('Function normalizes urls', () => {
-		expect(normalizeURL("http://blog.boot.dev/path/")).toBe(normalized);
-	});
-	test('Function normalizes urls', () => {
-		expect(normalizeURL("http://blog.boot.dev/path")).toBe(normalized);
-	});
-	test('Function normalizes urls', () => {
-		expect(normalizeURL("http://blog.boot.dev")).toBe("blog.boot.dev");
-	});
-	test('Throws on bad URL', () => {
-		expect(() => {
-			normalizeURL("")
-		}).toThrowError("Invalid URL");
-	})
+    const base = 'http://boot.dev';
+    const normalized = 'http://blog.boot.dev/path';
+    test('Function normalizes urls', () => {
+        expect(normalizeURL('http://blog.boot.dev/path/wow', base)).toBe(
+            normalized + '/wow'
+        );
+    });
+    test('Function normalizes urls', () => {
+        expect(normalizeURL('http://blog.boot.dev/path/', base)).toBe(
+            normalized
+        );
+    });
+    test('Function normalizes urls', () => {
+        expect(normalizeURL('http://blog.boot.dev', base)).toBe(
+            'http://blog.boot.dev'
+        );
+    });
+    test('Throws on bad URL', () => {
+        expect(() => {
+            normalizeURL('', base);
+        }).toThrowError('Invalid URL');
+    });
 });
 
 describe('Can get anchors from htmml', () => {
-	const base = "https://blog.boot.dev"
-	test("It can get an array of items", () => {
-		expect(getURLsFromHTML('<a></a>', base)).toHaveLength(1)
-	})
-	test("It can get an <a> tag", () => {
-		expect(getURLsFromHTML(`
+    const base = 'https://blog.boot.dev';
+    test('It can get an array of items', () => {
+        expect(getURLsFromHTML('<a></a>', base)).toHaveLength(1);
+    });
+    test('It can get an <a> tag', () => {
+        expect(
+            getURLsFromHTML(
+                `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -45,11 +48,15 @@ describe('Can get anchors from htmml', () => {
 </body>
 </html>
 		`,
-			base)[0]).toEqual(base + "/test")
-	})
-	test('Gets multiple a tags from a proper HTML', () => {
-		const dom = new JSDOM();
-		expect(getURLsFromHTML(`
+                base
+            )[0]
+        ).toEqual(base + '/test');
+    });
+    test('Gets multiple a tags from a proper HTML', () => {
+        const dom = new JSDOM();
+        expect(
+            getURLsFromHTML(
+                `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -61,7 +68,9 @@ describe('Can get anchors from htmml', () => {
 <a href="/test2"></a>
 </body>
 </html>
-		`, base)).toHaveLength(2)
-	})
-
-})
+		`,
+                base
+            )
+        ).toHaveLength(2);
+    });
+});
